b41c691e4d0725902d33c69378240b8c5d83b3c2
Tomáš Batelka <tomas.batelka@email.cz>
2020-04-29 15:30:04 +0200

    port-probe update v0.9

--- port-probe.sh
+++ port-probe.sh
@@ -3,7 +3,7 @@
 SHORT_SLEEP=2
 LONG_SLEEP=10
 
-BLUE='\e[0;34m'
+YELLOW='\e[0;11m'
 RED='\e[0;31m'
 GREEN='\e[0;32m'
 NC='\e[0m'
@@ -12,7 +12,7 @@ exitCheck () {
     read -n 1 -t 0.1 input
     if [[ $input = "q" ]] || [[ $input = "Q" ]]
     then
-        printf "\n\nSetting interface ${GREEN}${INTERFACE}${NC} up\n"
+        printf "\n\nSetting interface ${INTERFACE} ${GREEN}up${NC}\n"
         ip link set dev ${INTERFACE} up
         printf "\nExiting and thank you for using my script ${RED}♥${NC}\n\n"
         exit 1
@@ -20,21 +20,21 @@ exitCheck () {
 }
 
 printf "
-Starting ${BLUE}PortProbe${NC} script by ${BLUE}VOFY | TECHNOLOGIES (Tomáš Batelka)${NC}
+Starting ${YELLOW}PortProbe${NC} script by ${YELLOW}VOFY | TECHNOLOGIES (Tomáš Batelka)${NC}
 ----------------------------------------------------------------
-For exit script press ${BLUE}q${NC} and wait if you break this script interface can still be set on down\n\n"
+For exit script press ${YELLOW}q${NC} and wait if you break this script interface can still be set on down\n\n"
 
 INTERFACE="$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')"
-printf "Wired interface detected ${BLUE}${INTERFACE}${NC}\n\n"
+printf "Wired interface detected ${YELLOW}${INTERFACE}${NC}\n\n"
 
 while true
 do
-	printf "Setting interface ${RED}${INTERFACE}${NC} down      - ${SHORT_SLEEP}s\n"
+	printf "Setting interface ${INTERFACE} ${RED}down${NC}      - ${SHORT_SLEEP}s\n"
     ip link set dev ${INTERFACE} down
     sleep ${SHORT_SLEEP}
     exitCheck
     
-    printf "Setting interface $GREEN${INTERFACE}${NC} up        - ${LONG_SLEEP}s\n\n"
+    printf "Setting interface ${INTERFACE} ${GREEN}up${NC}        - ${LONG_SLEEP}s\n\n"
     ip link set dev ${INTERFACE} up
     sleep ${LONG_SLEEP}
     exitCheck
